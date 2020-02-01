aabdf54b8c03f14828ba2166b95871b8e7d5effe
Anarcker <anchieta.junior@gmail.com>
2020-02-01 14:36:02 -0300

    Adicionar mock animações do personagem principal

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,17 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement;
 
     public Rigidbody2D rigidBody;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()

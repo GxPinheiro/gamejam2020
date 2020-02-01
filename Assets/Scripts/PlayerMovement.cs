f159bee5fe9dfbdda0cb8f10e690bb4328a269b7
GxPinheiro <pinheiro.gbr@gmail.com>
2020-02-01 11:31:54 -0300

    adicionado trava de movimento quando o personagem estiver realizando ação

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,28 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public float moveSpeed = 5f;
     Vector2 movement;
+    private bool freezePlayer = false;
+    PlayerController playerController;
 
     public Rigidbody2D rigidBody;
 
+    void Start() {
+        GameObject thePlayer = GameObject.Find("player");
+        playerController = thePlayer.GetComponent<PlayerController>();
+        
+    }
     // Update is called once per frame
     void Update()
     {
+        freezePlayer = playerController.doingAction;
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate()
     {
-        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (!freezePlayer) {
+            rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 }

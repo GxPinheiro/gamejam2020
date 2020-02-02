a7574cb6d11ba84b9b2b6247eec7b3c8bc380bc9
Anarcker <anchieta.junior@gmail.com>
2020-02-02 02:29:37 -0300

    Animacao do caldeirao

--- Assets/Scripts/CauldronController.cs
+++ Assets/Scripts/CauldronController.cs
@@ -5,33 +5,44 @@ using UnityEngine;
 public class CauldronController : MonoBehaviour
 {
     public float actionTimer = 2f;
-    private bool doingAction = false;
 
+    private bool doingAction = false;
     private bool enableAction = false;
+    private bool potionReady = false;
 
     PlayerController playerController;
+    public Animator animator;
 
     void Start() {
         GameObject thePlayer = GameObject.Find("player");
         playerController = thePlayer.GetComponent<PlayerController>();
+
     }
     void Update()
     {
         enableAction = playerController.isHoldingItem;
-        
 
-        if (doingAction) {
+
+        if (doingAction)
+        {
             CalculateCountdown();
         }
 
-        if (Input.GetButtonDown("Fire2") && enableAction) {
+        if (Input.GetButtonDown("Fire2") && enableAction)
+        {
             doingAction = true;
+            animator.SetBool("Empty", false);
+            animator.SetBool("Cooking", true);
             enableAction = false;
             CalculateCountdown();
         }
-        
-    }
 
+        if (Input.GetButtonDown("Fire1"))  //debug sem && enableAction
+        {
+            animator.SetBool("Empty", true);
+            animator.SetBool("Cooking", false);
+        }
+    }
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player") {
@@ -49,6 +60,9 @@ public class CauldronController : MonoBehaviour
         if(actionTimer < 0){
             doingAction = false;
             actionTimer = 5f;
+            potionReady = true;
+            animator.SetBool("Empty", false);
+            animator.SetBool("Cooking", false);
             Debug.Log("Done");
         }
     }
